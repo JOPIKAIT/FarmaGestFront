bf99eb87fb17594c351f791e89d03dd7a6e29dc6
Pinto Kabonda <joaopintouan@gmail.com>
2025-11-25 10:30:14 +0100

    primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './system/feature/security/login/login.component';
+import { MainComponent } from './system/shared/components/main/main.component';
+import { DashboardComponent } from './system/feature/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'farmagest/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        component: LoginComponent,
+    },
+    {
+        path: 'farmagest',
+        component: MainComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
+                //canActivate: [authGuard],
+            },
+        ]
+    }
+];
